91f47944508a065fe0e90ed4db287a97e93e0816
daliacode <daliacode@gmail.com>
2023-04-08 17:10:27 +0200

    An example of changing ownership. Add a reference to my_vec into the loop instead

--- src/ownership_examples/vector_in_a_for_loop.rs
+++ src/ownership_examples/vector_in_a_for_loop.rs
@@ -1,7 +1,7 @@
 pub fn vector_in_a_for_loop() {
     let mut my_vec: Vec<i32> = vec![1,2,3,4,5];
 
-    for item in my_vec.iter() {  // the iter() function avoid to change the ownership
+    for item in &my_vec {  // another way is to pass only the reference into the loop-scope
         println!("{}", item);
     }
 
