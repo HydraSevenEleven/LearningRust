30484c24223c050f343f209beea8583bf7e1097c
daliacode <daliacode@gmail.com>
2023-04-08 17:19:56 +0200

    An example of changing ownership. Another way of solving this issue is to use the map function (which is lazy)

--- src/ownership_examples/vector_in_a_for_loop.rs
+++ src/ownership_examples/vector_in_a_for_loop.rs
@@ -1,9 +1,12 @@
 pub fn vector_in_a_for_loop() {
     let mut my_vec: Vec<i32> = vec![1,2,3,4,5];
 
-    for item in &my_vec {  // another way is to pass only the reference into the loop-scope
-        println!("{}", item);
-    }
-
+    my_vec
+    .iter()
+    .map(|item|{
+        println!("{:?}", item);
+    })
+    .for_each(drop);
+    
     println!("{:?}", my_vec);  
 }
