be474560992f690e4b40ded287fa790282b0b0a0
daliacode <daliacode@gmail.com>
2023-04-08 17:44:30 +0200

    fix the issue

--- src/ownership_examples/vector_in_a_for_loop.rs
+++ src/ownership_examples/vector_in_a_for_loop.rs
@@ -1,13 +1,13 @@
 pub fn vector_in_a_for_loop() {
-    let my_vec: Vec<i32> = vec![1,2,3,4,5];
+    let mut my_vec: Vec<i32> = vec![1,2,3,4,5];
 
     my_vec
-    .iter()
+    .iter_mut()
     .map(|item|{
-        *item += 1;  // item is a reference to the value. That means I cannot change the value of item 
+        *item += 1;  // To do this I need to define my_vec as mutable and use the iter_mut() fuunction 
         println!("{:?}", item);
     })
     .for_each(drop);
     
-    println!("{:?}", my_vec);  
+    println!("{:?}", my_vec);  // my_vec has changed
 }
